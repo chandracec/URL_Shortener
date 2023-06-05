9ddbdeda1c13665aaf03642b215679a5955d677e
jasmeendesai <jasmeendesai2597@gmail.com>
2023-06-05 15:57:27 +0530

    corrected status code

--- src/controller/control.js
+++ src/controller/control.js
@@ -17,11 +17,12 @@ client.on("connect", () => console.log("Connected to Redis"));
 const setCache = promisify(client.SET).bind(client);
 const getCache = promisify(client.GET).bind(client);
 
+
 //=========================================SHORT URL===================================================
 const shortUrl = async (req, res) => {
   try {
     const longUrl = req.body.longUrl;
-
+  
     if (!longUrl) {
       return res
         .status(400)
@@ -32,52 +33,49 @@ const shortUrl = async (req, res) => {
       return res.status(400).send({ status: false, message: "Invalid URL" });
     }
 
-    const baseUrl = "http://localhost:3000/";
+    const localCheck = await urlModel.findOne({ shortUrl: longUrl });
 
-    if (!baseUrl) {
+    if (localCheck) {
       return res
         .status(400)
-        .send({
-          status: false,
-          message: "Invalid request, please provide a baseUrl",
-        });
+        .send({ status: false, msg: "This URL is already shorted" });
     }
+
+    const baseUrl = "http://localhost:3000/";
 
     let getDataCache = await getCache(longUrl);
     getDataCache = JSON.parse(getDataCache);
 
     if (getDataCache) {
-      return res
-        .status(200)
-        .send({
-          status: true,
-          message: "URL exists in cache",
-          data: getDataCache,
-        });
+      return res.status(200).send({
+        status: true,
+        message: "URL exists in cache",
+        data: getDataCache,
+      });
     }
 
     const urlExists = await urlModel.findOne({ longUrl }, { _id: 0, __v: 0 });
 
     if (urlExists) {
-      await setCache(longUrl, JSON.stringify(urlExists), "EX", 100000);
-      return res
-        .status(200)
-        .send({
-          status: true,
-          message: "URL exists in database",
-          data: urlExists,
-        });
+      await setCache(longUrl, JSON.stringify(urlExists), "EX", 86400);
+      return res.status(200).send({
+        status: true,
+        message: "URL exists in database",
+        data: urlExists,
+      });
     }
 
     const urlCode = shortId.generate(longUrl);
     const shortUrl = `${baseUrl}${urlCode}`;
 
     const data = await urlModel.create({ longUrl, shortUrl, urlCode });
+
     const { _id, __v, ...Data } = data._doc;
 
-    await setCache(urlCode, JSON.stringify(Data), "EX", 100000);
+    //set cache
+    await setCache(longUrl, JSON.stringify(Data), "EX", 86400);
 
-    return res.status(200).send({ status: true, data: Data });
+    return res.status(201).send({ status: true, data: Data });
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
   }
@@ -88,14 +86,8 @@ const getUrl = async (req, res) => {
   try {
     const urlCode = req.params.urlCode;
 
-    if (!urlCode) {
-      return res
-        .status(400)
-        .send({ status: false, message: "Please provide a urlCode" });
-    }
-
     let getDataCache = await getCache(urlCode);
-
+    
     if (getDataCache) {
       const url = JSON.parse(getDataCache);
       return res.status(302).redirect(url.longUrl);
@@ -104,15 +96,13 @@ const getUrl = async (req, res) => {
     const url = await urlModel.findOne({ urlCode }, { _id: 0, __v: 0 });
 
     if (!url) {
-      return res
-        .status(400)
-        .send({
-          status: false,
-          message: `URL not found with the code ${urlCode}`,
-        });
+      return res.status(404).send({
+        status: false,
+        message: `URL not found with the code ${urlCode}`,
+      });
     }
 
-    await setCache(urlCode, JSON.stringify(url), "EX", 100000);
+    await setCache(urlCode, JSON.stringify(url), "EX", 86400);
     return res.status(302).redirect(url.longUrl);
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
